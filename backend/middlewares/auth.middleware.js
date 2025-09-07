638c1cdd5ff1675991e06f16a5908056203350b6
Vaibhav Patel <patelvaibhav122003@gmail.com>
2025-09-07 18:28:42 +0530

    Error Fix - 7

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,23 +1,25 @@
 import User from "../models/User.js";
 
 // Middleware to check if user is authenticated
-
 export const protect = async (req, res, next) => {
+  // ✅ Skip auth for preflight
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
   const userId = req.auth()?.userId;
   console.log(req.auth());
 
-  if(!userId) {
-    return res.status(401).json({success: false, message: "User is not authenticated"});
-  } else {
-    const user = await User.findById(userId);
-
-    if(!user) {
-      return res.status(401).json({success: false, message: "User not found"});
-    }
-
-    req.user = user;
-    next();
-
-
+  if (!userId) {
+    return res.status(401).json({ success: false, message: "User is not authenticated" });
   }
-}
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(401).json({ success: false, message: "User not found" });
+  }
+
+  req.user = user;
+  next();
+};
